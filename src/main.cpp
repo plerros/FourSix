152e7f9a935772fe86a8af3c7f5eb3e8795c93dd
sdi1700254 <sdi1700254@di.uoa.gr>
2024-10-14 01:35:50 +0300

    recursive steiner function

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,16 @@
 #include <boost/json.hpp>
 #include <boost/json/src.hpp>
+#include <chrono>
 #include <iostream>
+#include <CGAL/Delaunay_mesh_size_criteria_2.h>
+#include <CGAL/Delaunay_mesher_2.h>
+#include <CGAL/Line_2.h>
 
 #include "configuration.hpp"
 
 #include "json.hpp"
 #include "data_in.hpp"
 #include "data.hpp"
-
-#include <CGAL/Delaunay_mesh_size_criteria_2.h>
-#include <CGAL/Delaunay_mesher_2.h>
-#include <CGAL/Line_2.h>
 
 typedef CGAL::Delaunay_mesh_size_criteria_2<CDT> Criteria;
 typedef CGAL::Delaunay_mesher_2<CDT, Criteria>   Mesher;
@@ -51,32 +51,39 @@ size_t count_obtuse(CDT *cdt)
 	return ret;
 }
 
-void steiner_random(CDT *cdt)
+struct triangulation_t
+{
+	CDT cdt;
+	size_t obtuse;
+};
+
+void steiner_random(struct triangulation_t *ptr)
 {
 	std::vector<Point> random_pts;
 	{
-		CDT cdt2 = *cdt;
+		CDT cdt2 = ptr->cdt;
 		Mesher mesher(cdt2);
 		mesher.refine_mesh();
 		CGAL::Random_points_in_triangle_mesh_2<Point, CDT> generator(cdt2);
-		std::copy_n(generator, 1000, std::back_inserter(random_pts));
-		assert(random_pts.size() == 1000);
+		std::copy_n(generator, 100, std::back_inserter(random_pts));
+		assert(random_pts.size() == 100);
 	}
 
 	for (size_t i = 0; i < random_pts.size(); i++) {
-		CDT cdt2 = *cdt;
-		cdt2.insert(random_pts[i]);
-		if (count_obtuse(&cdt2) < count_obtuse(cdt))
-			cdt->insert(random_pts[i]);
+		struct triangulation_t current = *ptr;
+		current.cdt.insert(random_pts[i]);
+		current.obtuse = count_obtuse(&(current.cdt));
+		if (current.obtuse < ptr->obtuse)
+			*ptr = current;
 	}
 }
 
-void steiner_projection(CDT *cdt)
+void steiner_projection(struct triangulation_t *ptr)
 {
 	std::vector<Point> steiner_pts;
 
-	for (auto it = cdt->finite_faces_begin(); it != cdt->finite_faces_end(); it++) {
-		auto triangle = cdt->triangle(it);
+	for (auto it = ptr->cdt.finite_faces_begin(); it != ptr->cdt.finite_faces_end(); it++) {
+		auto triangle = ptr->cdt.triangle(it);
 		std::array <CGAL::Vector_2<K>, 3> edge;
 
 		for (size_t i = 0; i < 3; i++) {
@@ -114,15 +121,16 @@ void steiner_projection(CDT *cdt)
 		}
 	}
 	for (size_t i = 0; i < steiner_pts.size(); i++)
-		cdt->insert(steiner_pts[i]);
+		ptr->cdt.insert(steiner_pts[i]);
+	ptr->obtuse = count_obtuse(&(ptr->cdt));
 }
 
-void steiner_circumcenter(CDT *cdt, data_t *data)
+void steiner_circumcenter(struct triangulation_t *ptr, data_t *data)
 {
 	std::vector<Point> steiner_pts;
 
-	for (auto it = cdt->finite_faces_begin(); it != cdt->finite_faces_end(); it++) {
-		auto triangle = cdt->triangle(it);
+	for (auto it = ptr->cdt.finite_faces_begin(); it != ptr->cdt.finite_faces_end(); it++) {
+		auto triangle = ptr->cdt.triangle(it);
 		std::array <CGAL::Vector_2<K>, 3> edge;
 
 		Point steiner = CGAL::circumcenter(triangle);
@@ -131,32 +139,67 @@ void steiner_circumcenter(CDT *cdt, data_t *data)
 		
 		steiner_pts.push_back(steiner);
 	}
+
 	for (size_t i = 0; i < steiner_pts.size(); i++) {
-		CDT cdt2 = *cdt;
-		cdt2.insert(steiner_pts[i]);
-		if (count_obtuse(cdt) > count_obtuse(&cdt2))
-			*cdt = cdt2;
+		struct triangulation_t current = *ptr;
+		current.cdt.insert(steiner_pts[i]);
+		current.obtuse = count_obtuse(&(current.cdt));
+		if (ptr->obtuse > current.obtuse)
+			*ptr = current;
 	}
 }
 
-void steiner_random_projection(CDT *ptr, data_t *data, unsigned int retry)
+void steiner_mixed(struct triangulation_t *ptr, data_t *data, unsigned int retry)
 {
-	CDT original = *ptr;
+	struct triangulation_t original = *ptr;
 	for (unsigned int i = 0; i < retry; i++) {
-		CDT cdt = original;
-		steiner_random(&cdt);
+		struct triangulation_t current = original;
+		steiner_random(&current);
 		for (int j = 0; j < 20; j++) {
-			CDT cdt2 = cdt;
-			steiner_circumcenter(&cdt, data);
-			steiner_projection(&cdt2);
-			if (count_obtuse(&cdt) > count_obtuse(&cdt2))
-				cdt = cdt2;
-			//else if (count_obtuse(&cdt) < count_obtuse(&cdt2))
-			//	break;
+			struct triangulation_t current2 = current;
+			steiner_circumcenter(&current2, data);
+			steiner_projection(&(current2));
+			if (current.obtuse > current2.obtuse)
+				current = current2;
 		}
-		if (count_obtuse(ptr) > count_obtuse(&cdt))
-			*ptr = cdt;
+		if (ptr->obtuse > current.obtuse)
+			*ptr = current;
 	}
+}
+
+void steiner_mixed_recursive(struct triangulation_t *ptr, data_t *data, unsigned int depth)
+{
+	if (depth == 0)
+		return;
+
+	struct triangulation_t best = *ptr;
+	struct triangulation_t current = *ptr;
+	steiner_circumcenter(&current, data);
+	steiner_mixed_recursive(&current, data, depth - 1);
+	if (current.obtuse == 0) {
+		*ptr = current;
+		return;
+	}
+	if (best.obtuse > current.obtuse)
+		best = current;
+
+	current = *ptr;
+	steiner_projection(&current);
+	steiner_mixed_recursive(&current, data, depth - 1);
+	if (current.obtuse == 0) {
+		*ptr = current;
+		return;
+	}
+	if (best.obtuse > current.obtuse)
+		best = current;
+/*
+	current = *ptr;
+	steiner_random(&current);
+	steiner_mixed_recursive(&current, data, depth - 1);
+	if (best.obtuse > current.obtuse)
+		best = current;
+*/
+	*ptr = best;
 }
 
 int main(int argc, char** argv) {
@@ -186,12 +229,30 @@ int main(int argc, char** argv) {
 		for (const auto& constraint : data.get_constraints())
 			cdt.insert_constraint(constraint.first, constraint.second);
 
+		struct triangulation_t triangulation;
+		triangulation.cdt = cdt;
+		triangulation.obtuse = count_obtuse(&cdt); 
+
 		//CGAL::make_conforming_Delaunay_2(cdt);
 		//CGAL::make_conforming_Gabriel_2(cdt);
-		steiner_random_projection(&cdt, &data, 5);
+		//steiner_mixed(&cdt, &data, 5);
+		using std::chrono::high_resolution_clock;
+		using std::chrono::duration_cast;
+		using std::chrono::duration;
+		using std::chrono::milliseconds;
+		auto t1 = high_resolution_clock::now();
 
-		std::cout << count_obtuse(&cdt) << std::endl;
-		CGAL::draw(cdt);	
+		//steiner_mixed(&triangulation, &data, 5);
+		steiner_mixed_recursive(&triangulation, &data, triangulation.obtuse);
+
+		auto t2 = high_resolution_clock::now();
+		auto ms_int = duration_cast<milliseconds>(t2 - t1);
+		duration<double, std::milli> ms_double = t2 - t1;
+		std::cout << ms_int.count() << "ms\n";
+		std::cout << ms_double.count() << "ms\n";
+
+		std::cout << count_obtuse(&(triangulation.cdt)) << std::endl;
+		CGAL::draw(triangulation.cdt);	
 	}
 	catch(std::exception const& e) {
 		std::cerr <<
