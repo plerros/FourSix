3e9904c8095200f3bd87420a8ba9ace34faaf81f
sdi1700254 <sdi1700254@di.uoa.gr>
2024-12-24 21:57:48 +0200

    better parameter values

--- test/find_and_replace.sh
+++ test/find_and_replace.sh
@@ -4,11 +4,11 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 #find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/{"L":.*[0-9]*, "alpha":.*[0-9]*.[0-9]*, "beta":.*[0-9]*.[0-9]*}/{"L": 100, "alpha": 5.0, "beta": 0.2}/g' {} \;
 
-find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/"L":[0-9]*}/"L": 100}/g' {} \;
-find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/"L": [0-9]*}/"L": 100}/g' {} \;
+find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/"L":[0-9]*}/"L": 200}/g' {} \;
+find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/"L": [0-9]*}/"L": 200}/g' {} \;
 
-find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/"L":[0-9]*,/"L": 100,/g' {} \;
-find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/"L": [0-9]*,/"L": 100,/g' {} \;
+find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/"L":[0-9]*,/"L": 200,/g' {} \;
+find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/"L": [0-9]*,/"L": 200,/g' {} \;
 
 find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/"alpha":[0-9]*.[0-9]*,/"alpha": 5.0,/g' {} \;
 find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/"alpha": [0-9]*.[0-9]*,/"alpha": 5.0,/g' {} \;
