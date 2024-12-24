af1cbe012b574369d87c580a977f98b85e67bce3
sdi1700254 <sdi1700254@di.uoa.gr>
2024-12-24 19:19:53 +0200

    more tests & sync parameter values

--- test/find_and_replace.sh
+++ test/find_and_replace.sh
@@ -2,4 +2,31 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/"method": "ant",//g' {} \;
+#find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/{"L":.*[0-9]*, "alpha":.*[0-9]*.[0-9]*, "beta":.*[0-9]*.[0-9]*}/{"L": 100, "alpha": 5.0, "beta": 0.2}/g' {} \;
+
+find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/"L":[0-9]*}/"L": 100}/g' {} \;
+find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/"L": [0-9]*}/"L": 100}/g' {} \;
+
+find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/"L":[0-9]*,/"L": 100,/g' {} \;
+find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/"L": [0-9]*,/"L": 100,/g' {} \;
+
+find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/"alpha":[0-9]*.[0-9]*,/"alpha": 5.0,/g' {} \;
+find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/"alpha": [0-9]*.[0-9]*,/"alpha": 5.0,/g' {} \;
+
+find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/"beta":[0-9]*.[0-9]*}/"beta": 0.2}/g' {} \;
+find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/"beta": [0-9]*.[0-9]*}/"beta": 0.2}/g' {} \;
+
+find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/"beta":[0-9]*.[0-9]*,/"beta": 0.2,/g' {} \;
+find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/"beta": [0-9]*.[0-9]*,/"beta": 0.2,/g' {} \;
+
+find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/"xi":[0-9]*.[0-9]*,/"xi": 1.0,/g' {} \;
+find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/"xi": [0-9]*.[0-9]*,/"xi": 1.0,/g' {} \;
+
+find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/"psi":[0-9]*.[0-9]*,/"psi": 3.0,/g' {} \;
+find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/"psi": [0-9]*.[0-9]*,/"psi": 3.0,/g' {} \;
+
+find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/"lambda":[0-9]*.[0-9]*,/"lambda": 0.5,/g' {} \;
+find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/"lambda": [0-9]*.[0-9]*,/"lambda": 0.5,/g' {} \;
+
+find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/"kappa":[0-9]*}/"kappa": 10}/g' {} \;
+find "$SCRIPT_DIR/autodetect" -type  f -name "*.json" -exec sed -i 's/"kappa": [0-9]*}/"kappa": 10}/g' {} \;
